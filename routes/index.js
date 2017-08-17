4431aea81b887dd6e8a5cc6b0ef476b434ef1e7d
jiangliu2u <jiangliu2u@gmail.com>
2017-08-17 22:22:45 +0800

    add sessions and flash()

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',function (req,res) {
+    res.render('index', {
+        user : req.session.user,
+        success : req.flash('success').toString(),
+        error : req.flash('error').toString()
+    });
 });
+
 
 module.exports = router;
