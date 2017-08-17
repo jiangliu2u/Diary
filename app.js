de207a071eec9dfb89227b99f5cfa0e179d7cafc
jiangliu2u <jiangliu2u@gmail.com>
2017-08-17 22:17:17 +0800

    add sessions

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var reg=require('./routes/reg');
 var login=require('./routes/login');
 var MongoStore=require('connect-mongo')(session);
-
+var flash=require('connect-flash');
 var app = express();
 
 // view engine setup
@@ -24,6 +24,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 mongoose.connect("mongodb://localhost:27017/dialog");
 app.use(session({
@@ -40,6 +41,15 @@ app.use('/users', users);
 app.use('/reg',reg);
 app.use('/login',login);
 
+
+app.use(function(req, res, next){
+    console.log("app.usr local");
+    res.locals.user = req.session.user;
+    res.locals.post = req.session.post;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash(success);
+    next();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var model=require('../models/db');
+var model = require('../models/db');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('login');
@@ -15,15 +15,26 @@ router.post('/', function (req, res) {
         username: req.body.username,
         password: req.body.password
     };
-    model.user.find({'username':data.username,'password':data.password},function (err,docs) {
-       if(err){
-           res.redirect('/')
-       }else {
-           console.log(docs);
-           req.session.user=docs[0].username;
-           req.session.UserID=docs[0]._id;
-           res.redirect('/users')
-       }
+    model.user.find({'username': data.username}, function (err, user) {
+        console.log(user);
+        if (!user[0]) {
+            req.flash('error', '用户不存在');
+            console.log("用户不存在");
+            return res.redirect('/login');
+        }
+
+        if (req.body.password != user[0].password) {
+            console.log("输入的密码"+req.body.password);
+            console.log("数据库的密码"+user[0].password);
+            console.log("密码错误");
+            req.flash('error', '用户名或密码错误');
+            return res.redirect('/login');
+        }
+        req.session.user = user[0];
+        console.log("sssss");
+        req.flash('success', req.session.user.username + '登录成功');
+        console.log('用户名'+req.session.user.username);
+        res.redirect('/');
     });
 
 });
