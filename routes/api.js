d8f1031080b5b7413ac35d76c40f0a4fe6864717
jiangliu2u <jiangliu2u@gmail.com>
2017-08-28 21:04:56 +0800

    解决跨域访问问题，允许本地angular应用开发过程中跨域访问另一个express应用的数据

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const model = require('../models/db');
 /* GET home page. */
 
+//解决跨域访问问题，允许angular对本服务器进行访问。
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+};
+router.use(allowCrossDomain);
 router.get('/', function (req, res) {
-
     model.dialog.find(function (err, posts) {
         let post_s = [];
         if (err) {
