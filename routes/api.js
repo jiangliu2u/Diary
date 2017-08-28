255c5a21af2f14541eeb2ead573f12faed1f9819
jiangliu2u <jiangliu2u@gmail.com>
2017-08-28 23:02:53 +0800

    解决跨域访问问题，允许本地angular应用开发过程中跨域访问另一个express应用的数据

--- routes/api.js
+++ routes/api.js
@@ -16,9 +16,8 @@ router.get('/', function (req, res) {
     model.dialog.find(function (err, posts) {
         let post_s = [];
         if (err) {
-            console.log('读取post出错' + err);
+            console.log('读取post出错:' + err);
         } else {
-            console.log('哈哈哈' + {posts});
             post_s = posts;
             res.json(post_s);
         }
